ccd65cc6f83ebdacf72089b8bb50177610b7060b
Igor <igor.trevisan@aluno.faculdadeimpacta.com.br>
2020-10-01 22:23:27 -0300

    ac3

--- primos.py
+++ primos.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def nao_entre_em_panico():
@@ -30,6 +30,6 @@ def nao_entre_em_panico():
 
     return primos
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
